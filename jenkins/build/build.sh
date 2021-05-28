d8491dacff7ba19e371046e2507064ee87007210
hiepph1707 <hiepph1707@gmail.com>
2021-05-28 23:19:07 +0700

    Updated build.sh and docker-compose

--- jenkins/build/build.sh
+++ jenkins/build/build.sh
@@ -1,11 +1,24 @@
 #!/bin/bash
 
-# Copy source code to the build location
-cp -f nodejs/* jenkins/build/
-
 echo "****************************"
 echo "** Building Docker Image ***"
 echo "****************************"
 
-cd jenkins/build/ && /usr/local/bin/docker-compose -f docker-compose-build.yml build #--no-cache
+if [ $1 == "python" ]
+then
+    # Copy source code to the build location
+    cp -ar python jenkins/build/
+    cd jenkins/build/ && /usr/local/bin/docker-compose -f docker-compose-python.yml build #--no-cache
+elif [ $1 == "nodejs" ]
+then
+    # Copy source code to the build location
+    cp -ar nodejs jenkins/build/
+    cd jenkins/build/ && /usr/local/bin/docker-compose -f docker-compose-nodejs.yml build #--no-cache
+else
+    cp -ar {python,nodejs} jenkins/build/
+    
+
+
+
+
 
