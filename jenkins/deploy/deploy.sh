fd3059a98e58d58c884050b98cd513bc584e2c91
hiepph1707 <hiepph1707@gmail.com>
2021-05-29 19:14:23 +0700

    gfd

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -2,32 +2,36 @@
 
 PYTHON_CT_NAME='python-app'
 NODEJS_CT_NAME='nodejs-app'
+
+removeOldContainer ($CONTAINER_NAME) {
+    echo "Remove old container"
+    if [ $(docker ps -aq --filter name=$CONTAINER_NAME) ]
+    then
+        docker rm -f $CONTAINER_NAME
+    fi
+}
+
 echo "****************************"
 echo "** Deploy application ***"
 echo "****************************"
-
-# Remove old python container
-if [ $(docker ps -aq --filter name=$PYTHON_CT_NAME) ]
-then
-    docker rm -f $PYTHON_CT_NAME
-fi
-
-# Remove old nodejs container
-if [ $(docker ps -aq --filter name=$NODEJS_CT_NAME) ]
-then
-    docker rm -f $NODEJS_CT_NAME
-fi
 
 # Login to Docker registry
 docker login -u hiepph1707 -p $PASS
 
 if [ $1 == "python" ]
 then
+    # Remove old python container
+    removeOldContainer($PYTHON_CT_NAME)
     docker run -d --name $PYTHON_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 5000:5000 hiepph1707/${IMAGE_PYTHON}:${IMAGE_TAG}
 elif [ $1 == "nodejs" ]
 then
+    # Remove old nodejs container
+    removeOldContainer($NODEJS_CT_NAME)
     docker run -d --name $NODEJS_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 3000:3000 hiepph1707/${IMAGE_NODE}:${IMAGE_TAG}
 else
+    # Remove old nodejs container
+    removeOldContainer($NODEJS_CT_NAME)
+    removeOldContainer($PYTHON_CT_NAME)
     docker run -d --name $NODEJS_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 3000:3000 hiepph1707/${IMAGE_NODE}:${IMAGE_TAG}
     docker run -d --name $PYTHON_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 5000:5000 hiepph1707/${IMAGE_PYTHON}:${IMAGE_TAG} 
 fi
