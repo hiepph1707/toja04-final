dae554983162684d69b7286be5b64fd06fb56eb4
hiepph1707 <hiepph1707@gmail.com>
2021-05-29 16:16:16 +0700

    Updated Jenkinsfile

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -4,13 +4,13 @@ echo "****************************"
 echo "** Deploy application ***"
 echo "****************************"
 
-/usr/bin/docker login -u hiepph1707 -p $PASS
+docker login -u hiepph1707 -p $PASS
 if [ $1 == "python" ]
 then
-    cd jenkins/deploy/ && /usr/bin/docker-compose -f docker-compose-python.yml up -d
+    cd jenkins/deploy/ && docker-compose -f docker-compose-python.yml up -d
 elif [ $1 == "nodejs" ]
 then
-    cd jenkins/deploy/ && /usr/bin/docker-compose -f docker-compose-nodejs.yml up -d
+    cd jenkins/deploy/ && docker-compose -f docker-compose-nodejs.yml up -d
 else
-    cd jenkins/deploy/ && /usr/bin/docker-compose -f docker-compose-all.yml up -d 
+    cd jenkins/deploy/ && docker-compose -f docker-compose-all.yml up -d 
 fi
