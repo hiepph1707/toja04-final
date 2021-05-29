8773b7fe03a8dbcdfbb3a48300cba13df940c8e1
hiepph1707 <hiepph1707@gmail.com>
2021-05-29 18:51:09 +0700

    fdff

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -23,15 +23,11 @@ docker login -u hiepph1707 -p $PASS
 
 if [ $1 == "python" ]
 then
-    docker login -u hiepph1707 -p $PASS
     docker run -d --name $PYTHON_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 5000:5000 hiepph1707/${IMAGE_PYTHON}:${IMAGE_TAG}
 elif [ $1 == "nodejs" ]
 then
-    docker login -u hiepph1707 -p $PASS
-    docker run -d --name $NODEJS_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 3000:3000 ${IMAGE_NODE}:${IMAGE_TAG}
+    docker run -d --name $NODEJS_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 3000:3000 hiepph1707/${IMAGE_NODE}:${IMAGE_TAG}
 else
-    docker login -u hiepph1707 -p $PASS
-    docker run -d --name $NODEJS_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 3000:3000 ${IMAGE_NODE}:${IMAGE_TAG}
-    docker login -u hiepph1707 -p $PASS
-    docker run -d --name $PYTHON_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 5000:5000 ${IMAGE_PYTHON}:${IMAGE_TAG} 
+    docker run -d --name $NODEJS_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 3000:3000 hiepph1707/${IMAGE_NODE}:${IMAGE_TAG}
+    docker run -d --name $PYTHON_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 5000:5000 hiepph1707/${IMAGE_PYTHON}:${IMAGE_TAG} 
 fi
