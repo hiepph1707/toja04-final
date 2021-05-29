6c7fdc9c5bb39adbbec53d2281d89be13346b74c
hiepph1707 <hiepph1707@gmail.com>
2021-05-29 18:53:43 +0700

    bccbc

--- python/index.py
+++ python/index.py
@@ -3,6 +3,6 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello World Python! Latest " + os.getenv('HOSTNAME')
+    return "Hello World Python! Latest" + " " + os.getenv('HOSTNAME')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
