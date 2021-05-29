e3cc7556327132a6b8c388467c799afe843a3f4b
hiepph1707 <hiepph1707@gmail.com>
2021-05-29 17:54:51 +0700

    fdffdfdf

--- jenkins/deploy/deploy.sh
+++ jenkins/deploy/deploy.sh
@@ -1,5 +1,7 @@
 #!/bin/bash
 
+PYTHON_CT_NAME='python-app'
+NODEJS_CT_NAME='nodejs-app'
 echo "****************************"
 echo "** Deploy application ***"
 echo "****************************"
@@ -7,10 +9,18 @@ echo "****************************"
 docker login -u hiepph1707 -p $PASS
 if [ $1 == "python" ]
 then
-    cd jenkins/deploy/ && /usr/local/bin/docker-compose -f docker-compose-python.yml up -d
+    if [ docker ps -aq --filter name=$PYTHON_CT_NAME ]
+    then
+        docker rm -f $PYTHON_CT_NAME
+    fi
+    docker run -d --name $PYTHON_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 5000:5000 hiepph1707/my-python:latest
 elif [ $1 == "nodejs" ]
 then
-    cd jenkins/deploy/ && /usr/local/bin/docker-compose -f docker-compose-nodejs.yml up -d
+    if [ docker ps -aq --filter name=$NODEJS_CT_NAME ]
+    then
+        docker rm -f $NODEJS_CT_NAME
+    fi
+    docker run -d --name $NODEJS_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 5000:5000 hiepph1707/my-nodejs:latest
 else
     cd jenkins/deploy/ && /usr/local/bin/docker-compose -f docker-compose-all.yml up -d 
 fi
