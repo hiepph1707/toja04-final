e4dc9af7e158654739f3d4f7c9bdbf852eb29f89
hiepph1707 <hiepph1707@gmail.com>
2021-05-30 10:39:04 +0700

    dsdsd

--- docker-image-update-check.sh
+++ docker-image-update-check.sh
@@ -13,11 +13,10 @@ removeOldContainer () {
     fi
 }
 
-echo "Fetching Docker Hub token..."
 token_python=$(curl --silent "https://auth.docker.io/token?scope=repository:$IMAGE_PYTHON:pull&service=registry.docker.io" | jq -r '.token')
 token_nodejs=$(curl --silent "https://auth.docker.io/token?scope=repository:$IMAGE_NODEJS:pull&service=registry.docker.io" | jq -r '.token')
 
-echo "Fetching remote digest... "
+echo "`date +%F" "%R:%S` Fetching remote digest... "
 digest_python=$(curl --silent -H "Accept: application/vnd.docker.distribution.manifest.v2+json" \
         -H "Authorization: Bearer $token_python" \
         "https://registry.hub.docker.com/v2/$IMAGE_PYTHON/manifests/latest" | jq -r '.config.digest')
@@ -33,15 +32,15 @@ local_digest_nodejs=$(docker images -q --no-trunc $IMAGE_NODEJS:latest)
 echo "Local digest NodeJs $local_digest_nodejs"
 
 if [ "$digest_python" != "$local_digest_python" ] ; then
-        echo "Pulling latest version $IMAGE_PYTHON..."
+        echo "`date +%F" "%R:%S` Pulling latest version $IMAGE_PYTHON..."
         docker pull $IMAGE_PYTHON
         removeOldContainer $PYTHON_CT_NAME
         docker run -d --name $PYTHON_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 5000:5000 ${IMAGE_PYTHON}
 elif [ "$digest_nodejs" != "$local_digest_nodejs"  ]; then
-        echo "Pulling latest version $IMAGE_NODEJS..."
+        echo "`date +%F" "%R:%S` Pulling latest version $IMAGE_NODEJS..."
         docker pull $IMAGE_NODEJS
         removeOldContainer $NODEJS_CT_NAME
         docker run -d --name $NODEJS_CT_NAME -e "HOSTNAME=$(hostname -f)" -p 3000:3000 ${IMAGE_NODEJS}
 else
-        echo "Already up to date. Nothing to do."
+        echo "`date +%F" "%R:%S` Already up to date. Nothing to do."
 fi
